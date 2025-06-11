4fc2ea86ff3040efad0c95f7187d552375c1eed9
chinnnn07 <chinnnn3107@gmail.com>
2025-06-11 22:13:56 +0700

    Upload code

---
+++ cbullet.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+#include "cpoint.h"
+
+class cbullet
+{
+    int _n, _speed;
+    cpoint _p[cpoint::MAP_ROW * cpoint::MAP_COL];
+    cpoint _m[cpoint::MAP_ROW][cpoint::MAP_COL];
+    cpoint _curr;
+
+public:
+    cbullet();
+    void updateMap(int i, int j, cpoint v);
+    cpoint getCurr() const;
+    void setCurr(const cpoint& tcurr);
+    cpoint* getP();
+    int getSpeed() const;
+    int getN() const;
+    void setN(int tn);
+    void setSpeed(int tspeed);
+    int queryCFromRowCol(int row, int col) const;
+    int calcPathBullet(const cpoint& tower);
+};
---
+++ ctower.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "cpoint.h"
+#include "cbullet.h"
+
+class ctower
+{
+    cpoint _location;
+    cbullet _cb;
+public:
+    ctower();
+    cpoint getLocation() const;
+    void setLocation(const cpoint& tl);
+    void setMapForBullet(cpoint map[][cpoint::MAP_COL]);
+    void calcPathBullet();
+    cbullet& getBullet();
+};
